b048af62ecfc3de1d396c3b017168fdafad9da5c
Pritipal Singh <peeyushsolanki22@gmai.com>
2024-11-26 00:37:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    function SendMail() {
+      event.preventDefault();
+    
+        const data = {
+          from_name: document.getElementById('firstname').value,
+          email_id: document.getElementById('email').value,
+          message: document.getElementById('messages').value
+          };
+    
+          
+           // Send email
+         emailjs.send('service_1sijuz9', 'template_0lorufc', data)
+         .then(function(response){
+           
+          alert("Message sent sucessfully...thankyou for reaching out!!" , response.status) ;
+          }, function(error){
+            alert("Message could not deliver!!" , error) ;
+          });
+      }
+  
+
+ 
+
+
